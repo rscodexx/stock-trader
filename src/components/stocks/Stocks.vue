09490f8682b8de2079f9bae4ff49310d26d574b3
Rafael Santos <raffrenan@gmail.com>
2022-04-05 09:05:53 -0300

    Add menu stock.

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>Ações</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id"></Stock>
+    </v-layout>
 </template>
 
 <script>
+
+import Stock from './Stock'
+
 export default {
-    name: "Stocks"
+    name: "Stocks",
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 110},
+                { id: 2, name: 'Google', price: 200},
+                { id: 3, name: 'Apple', price: 310},
+                { id: 4, name: 'Twitter', price: 165},
+            ]
+        }
+    }
 }
 </script>
 
