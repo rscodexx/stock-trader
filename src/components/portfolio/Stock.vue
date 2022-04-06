2ce4837f448fb5c1a634ccc23e09d9cd07c82b9f
Rafael Santos <raffrenan@gmail.com>
2022-04-05 23:18:56 -0300

    Add validates in the system.

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -10,10 +10,12 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
+                <v-text-field label="Quantidade" type="number"
+                              :error="insufficientQuantity || !Number.isInteger(quantity)"
+                              v-model.number="quantity"></v-text-field>
                 <v-btn class="blue darken-3 white--text"
-                       :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                       @click="sellStock">Vender</v-btn>
+                       :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+                       @click="sellStock">{{ insufficientQuantity? 'Insuficiente' : 'Vender' }}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -27,6 +29,11 @@ export default {
         return {
             quantity: 0
         }
+    },
+    computed: {
+        insufficientQuantity(){
+            return this.quantity > this.stock.quantity
+        },
     },
     methods: {
         sellStock(){
